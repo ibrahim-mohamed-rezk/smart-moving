97b3c6fdafb01eddade0022a69923202f828ee17
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2025-04-28 13:40:27 +0300

    connect home with api

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,35 +1,36 @@
-
 import Image from "next/image";
-import {
-  MoveRight,
-  Car,
-  Building2,
-  Armchair,
-  Database,
-  CarTaxiFront,
-} from "lucide-react";
+import { MoveRight } from "lucide-react";
 import logo from "../../../public/HeroSection.png";
 import or from "../../../public/OR.png";
 import image from "../../../public/image0.png";
 import BestCompanies from "@/components/home/BestCompanies";
 import LatestReviews from "@/components/home/LatestReviews";
 import { getData } from "@/libs/axios/server";
+import { ServiceTypes } from "@/libs/types/types";
 
-const HomePage = async() => {
+const HomePage = async ({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) => {
+  const { locale } = await params;
   const feachData = async () => {
     try {
-      const response = await getData("home");
+      const response = await getData(
+        "home",
+        {},
+        {
+          lang: locale,
+        }
+      );
       return response.data;
     } catch (error) {
-      throw error;
+      console.log(error);
     }
   };
 
-  const homeData = await feachData();
-
-  console.log(homeData);
-
-  
+  const { services, hero_section, about_us, companies, reviews } =
+    await feachData();
 
   return (
     <div className="bg-white">
@@ -37,23 +38,28 @@ const HomePage = async() => {
       <section className="bg-[#0E172F] text-white px-[clamp(1rem,5vw,4rem)] py-[clamp(2rem,6vw,5rem)]">
         <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-[clamp(2rem,5vw,5rem)]">
           <div>
-            <h2 className="text-[clamp(2.5rem,5vw,6rem)] md:text-4xl font-bold leading-snug font-['franklin-gothic-heavy']">
-              Get 3 free quotes for <br /> your move
-            </h2>
-            <ul className="list-disc pl-6 mt-6 space-y-2 text-[clamp(1rem,2vw,1.5rem)] text-gray font-['franklin-gothic-heavy'] font-bold">
-              <li>No commitment</li>
-              <li>Professional movers</li>
-              <li>Save up to 70%</li>
-            </ul>
+            <h2
+              dangerouslySetInnerHTML={{ __html: hero_section?.name }}
+              className="text-[clamp(2.5rem,5vw,6rem)] md:text-4xl font-bold leading-snug font-['franklin-gothic-heavy']"
+            />
+
+            <p
+              dangerouslySetInnerHTML={{
+                __html: hero_section?.description || "",
+              }}
+              className="list-disc pl-6 mt-6 space-y-2 text-[clamp(1rem,2vw,1.5rem)] text-gray font-['franklin-gothic-heavy'] font-bold"
+            />
+
             <button className="mt-6 inline-flex items-center gap-2 px-6 py-3 font-['libre-baskerville'] bg-[#0E1B4D] text-white text-sm rounded-full hover:bg-[#1c2a63] transition">
               <MoveRight className="w-4 h-4 " />
               Select your move now
             </button>
           </div>
           <Image
-            src={logo}
+            src={hero_section?.image || logo}
             alt="Smart Moving"
             width={700}
+            height={400}
             className="object-contain max-w-full"
           />
         </div>
@@ -65,45 +71,26 @@ const HomePage = async() => {
           services of move
         </h3>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 max-w-7xl mx-auto">
-          {[
-            {
-              title: "Company Relocation",
-              desc: "Professional office moving with zero business disruption",
-              icon: <Building2 className="w-6 h-6" />,
-            },
-            {
-              title: "Private Moving",
-              desc: "Moving tailored to your needs.",
-              icon: <Car className="w-6 h-6" />,
-            },
-            {
-              title: "Furniture Pick-up",
-              desc: "Stress-free full service.",
-              icon: <Armchair className="w-6 h-6" />,
-            },
-            {
-              title: "Storage",
-              desc: "Secure storage solutions.",
-              icon: <Database className="w-6 h-6" />,
-            },
-            {
-              title: "Taxi",
-              desc: "Quick delivery for small loads.",
-              icon: <CarTaxiFront className="w-6 h-6" />,
-            },
-          ].map((service, idx) => (
+          {services.map((service: ServiceTypes, idx: number) => (
             <div
               key={idx}
               className="relative bg-[#F3F4F6] p-10 rounded-xl text-center border shadow-md transition-all duration-300 group my-5 mx-3 hover:bg-[#0E172F] hover:text-white"
             >
               <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 w-16 h-16 bg-white border border-gray-200 rounded-full flex items-center justify-center shadow">
-                <div className="text-[#0E172F]">{service.icon}</div>
+                <div className="text-[#0E172F]">
+                  <Image
+                    src={service.image}
+                    alt="Service Logo"
+                    width={50}
+                    height={50}
+                  />
+                </div>
               </div>
               <h4 className="text-lg font-extrabold mt-10 mb-3 text-[#0E172F] group-hover:text-white transition-colors">
                 {service.title}
               </h4>
               <p className="text-sm text-gray-700 mb-6 max-w-xs mx-auto group-hover:text-white transition-colors">
-                {service.desc}
+                {service.description}
               </p>
               <div className="mx-auto w-10 h-10 rounded-full bg-white border border-gray-300 flex items-center justify-center transition-colors group-hover:bg-white group-hover:text-[#0E172F]">
                 <span className="text-2xl">→</span>
@@ -117,26 +104,29 @@ const HomePage = async() => {
       <section className="bg-[#F3F8FF] py-[clamp(2rem,6vw,5rem)] px-[clamp(1rem,5vw,4rem)]">
         <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
           <div>
-            <h3 className="text-3xl font-bold mb-4">About Us</h3>
-            <p className="text-base text-[#7FB63D] mb-4">
-              Smart Moving Services are reliable, safe, and fast moving partners
-              who help make your relocation stress-free. Our platform connects
-              you with top-rated companies.
-            </p>
+            <h3 className="text-3xl font-bold mb-4">{about_us.title}</h3>
+            <p
+              dangerouslySetInnerHTML={{ __html: about_us.description }}
+              className="text-base text-[#7FB63D] mb-4"
+            />
             <ul className="text-base space-y-4 text-[#7FB63D]">
-              {[
-                "Best deals",
-                "Fast responses",
-                "Professional support",
-                "Secure payments",
-              ].map((text, idx) => (
-                <li key={idx} className="flex items-center gap-3">
+              {Array.isArray(about_us.keywords) ? (
+                about_us.keywords.map((text: string, idx: number) => (
+                  <li key={idx} className="flex items-center gap-3">
+                    <div className="w-8 h-8 rounded-full bg-[#7FB63D] text-white flex items-center justify-center font-bold text-sm">
+                      {idx + 1}
+                    </div>
+                    <span>{text}</span>
+                  </li>
+                ))
+              ) : (
+                <li className="flex items-center gap-3">
                   <div className="w-8 h-8 rounded-full bg-[#7FB63D] text-white flex items-center justify-center font-bold text-sm">
-                    {idx + 1}
+                    1
                   </div>
-                  <span>{text}</span>
+                  <span>{about_us.keywords}</span>
                 </li>
-              ))}
+              )}
             </ul>
           </div>
           <div className="relative">
@@ -153,12 +143,12 @@ const HomePage = async() => {
       </section>
 
       {/* Best Companies Section */}
-      <BestCompanies />
+      <BestCompanies companies={companies} />
 
       {/* Latest Reviews Section */}
-      <LatestReviews />
+      <LatestReviews reviews={reviews} />
     </div>
   );
-}
+};
 
-export default HomePage
+export default HomePage;
