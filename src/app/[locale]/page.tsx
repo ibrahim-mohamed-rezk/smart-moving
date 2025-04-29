fcfc0573fcf5e396aaeb20a83318d804d8f4bd86
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2025-04-29 18:08:48 +0300

    services

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -33,12 +33,12 @@ const HomePage = async ({
   return (
     <div className="bg-white">
       {/* Hero Section */}
-      <section className="bg-[#0E172F] text-white px-[clamp(1rem,5vw,4rem)] py-[clamp(2rem,6vw,5rem)]">
+      <section className="bg-[#0F152F] text-white px-[clamp(1rem,5vw,4rem)] py-[clamp(2rem,6vw,5rem)]">
         <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-[clamp(2rem,5vw,5rem)]">
           <div>
             <h2
               dangerouslySetInnerHTML={{ __html: hero_section?.name }}
-              className="text-[clamp(2.5rem,5vw,6rem)] md:text-4xl font-bold leading-snug font-['franklin-gothic-heavy']"
+              className="text-[clamp(2.5rem,5vw,6rem)] capitalize md:text-4xl font-bold leading-snug font-['franklin-gothic-heavy']"
             />
 
             <p
@@ -65,33 +65,61 @@ const HomePage = async ({
 
       {/* Services Section */}
       <section className="text-center px-[clamp(1rem,5vw,4rem)] py-[clamp(2rem,6vw,4rem)]">
-        <h3 className="text-[clamp(2.5rem,5vw,4rem)] font-normal leading-[100%] tracking-[0] text-center font-['franklin-gothic-heavy'] mb-10">
+        <h3 className="text-[clamp(18px,3.333vw,164px)] text-[#192953] capitalize font-normal leading-[100%] tracking-[0] text-center font-['franklin-gothic-heavy'] mb-[clamp(20px,2.083vw,140px)]">
           services of move
         </h3>
-        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 max-w-7xl mx-auto">
+        <div className="flex items-center flex-wrap justify-center gap-[clamp(50px,5.208vw,1100px)] mx-auto">
           {services.map((service: ServiceTypes, idx: number) => (
             <div
               key={idx}
-              className="relative bg-[#F3F4F6] p-10 rounded-xl text-center border shadow-md transition-all duration-300 group my-5 mx-3 hover:bg-[#0E172F] hover:text-white"
+              data-property-1="default"
+              className="relative group flex flex-col items-center justify-center w-[clamp(250px,26.042vw,1500px)] h-[clamp(250px,23.438vw,1450px)]"
             >
-              <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 w-16 h-16 bg-white border border-gray-200 rounded-full flex items-center justify-center shadow">
-                <div className="text-[#0E172F]">
+              <div className="w-full h-full px-[clamp(5px,1.667vw,132px)] bg-zinc-100 rounded-2xl shadow-[8px_8px_16px_0px_rgba(0,0,0,0.16)] flex flex-col justify-center items-center gap-[clamp(10px,1.667vw,132px)] transition-all duration-300 hover:bg-[#0E172F]">
+                <div className="self-stretch flex flex-col justify-center items-center gap-3 sm:gap-4">
+                  <div className="text-blue-950 text-[clamp(18px,1.875vw,116px)] font-bold font-['Libre_Baskerville'] capitalize transition-colors group-hover:text-white hover:text-white">
+                    {service.title}
+                  </div>
+                  <div className="self-stretch text-center text-black/60 text-[clamp(14px,1.042vw,120px)] font-bold font-['Libre_Baskerville'] transition-colors group-hover:text-white/70 hover:text-white/70">
+                    {service.description}
+                  </div>
+                </div>
+                <div className="p-[clamp(5px,0.833vw,30px)] bg-white rounded-[100px] inline-flex justify-center items-center transition-colors hover:bg-white">
+                  <div className="w-[clamp(50px,3.5vw,148px)] h-[clamp(50px,3.5vw,148px)] relative flex items-center justify-center">
+                    <svg
+                      className="w-[clamp(50px,3.5vw,548px)] h-[clamp(50px,3.5vw,548px)]"
+                      viewBox="0 0 48 48"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M40.0002 23.9996H8.00024"
+                        stroke="#192953"
+                        stroke-width="3"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M30.0007 34C30.0007 34 40.0005 26.6352 40.0005 24C40.0005 21.3648 30.0005 14 30.0005 14"
+                        stroke="#192953"
+                        stroke-width="3"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[clamp(50px,5.0vw,296px)] -translate-y-1/2 h-[clamp(50px,5.0vw,296px)] p-[clamp(5px,0.833vw,30px)] absolute left-1/2 top-0 transform translate-x-[-50%] bg-zinc-100 rounded-[100px] shadow-[10px_10px_14px_0px_rgba(0,0,0,0.16)] flex flex-col justify-center items-center">
+                <div className="w-full h-full relative flex items-center justify-center">
                   <Image
                     src={service.image}
                     alt="Service Logo"
                     width={50}
                     height={50}
+                    className="object-contain w-full h-full"
                   />
                 </div>
-              </div>
-              <h4 className="text-lg font-extrabold mt-10 mb-3 text-[#0E172F] group-hover:text-white transition-colors">
-                {service.title}
-              </h4>
-              <p className="text-sm text-gray-700 mb-6 max-w-xs mx-auto group-hover:text-white transition-colors">
-                {service.description}
-              </p>
-              <div className="mx-auto w-10 h-10 rounded-full bg-white border border-gray-300 flex items-center justify-center transition-colors group-hover:bg-white group-hover:text-[#0E172F]">
-                <span className="text-2xl">→</span>
               </div>
             </div>
           ))}
@@ -102,7 +130,9 @@ const HomePage = async ({
       <section className="bg-[#F3F8FF] py-[clamp(2rem,6vw,5rem)] px-[clamp(1rem,5vw,4rem)]">
         <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
           <div>
-            <h3 className="text-3xl font-bold mb-4">{about_us.title}</h3>
+            <h3 className="text-3xl capitalize font-bold mb-4">
+              {about_us.title}
+            </h3>
             <p
               dangerouslySetInnerHTML={{ __html: about_us.description }}
               className="text-base text-[#7FB63D] mb-4"
@@ -131,6 +161,8 @@ const HomePage = async ({
             <Image
               src={"/OR.png"}
               alt="Or Section"
+              width={700}
+              height={400}
               className="w-full max-h-dvh"
             />
             <div className="absolute bottom-15 hidden lg:flex md:flex  left-0 p-8 border-8 rounded-full border-amber-50 bg-[#0F152F] -translate-x-1/2">
